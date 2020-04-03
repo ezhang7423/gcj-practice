0edd75b4a8ed04510d60a841eaa8457c2d99a352
ezhang7423 <ezhang7423@student.palomar.edu>
2020-04-02 22:33:07 -0700

    done?

--- 2018/savingtheuniverse.py
+++ 2018/savingtheuniverse.py
@@ -2,7 +2,7 @@ def calcDamage(st):
     charge = 1
     damage = 0
     for x in st:
-        if x == 'c':
+        if x == 'C':
             charge *= 2
         else:
             damage += charge
@@ -24,14 +24,17 @@ def findSwaps(tol, st):
 
 def swap(st):
     st = st[::-1]
-    first, second = ''
+    st = list(st)
+    first, second = ['', '']
     for i, x in enumerate(st):
         second = x
-        if (first + second == 'cs'):
+        if (first + second == 'SC'):
             tmp = st[i]
             st[i] = st[i-1]
             st[i-1] = tmp
-            return st[::-1]
+            return ''.join(st)[::-1]
+        else:
+            print(first + second)
         first = second
     return 'impossible'
 
@@ -39,8 +42,9 @@ def swap(st):
 t = int(input())  # read a line with a single integer
 for i in range(1, t + 1):
         # read a list of integers, 2 in this case
-    d = int(input().split(' ')[0])
-    st = input().split(' ')[1]
+    x = input()
+    d = int(x.split(' ')[0])
+    st = x.split(' ')[1]
     ans = findSwaps(d, st)
     if ans == -1:
         ans = "IMPOSSIBLE"
