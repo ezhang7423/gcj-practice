54173db9d6481d3e5f212915e08eb98e3231c134
ezhang7423 <ezhang7423@student.palomar.edu>
2020-04-02 22:22:29 -0700

    2018 sol

---
+++ 2018/savingtheuniverse.py
@@ -0,0 +1,48 @@
+def calcDamage(st):
+    charge = 1
+    damage = 0
+    for x in st:
+        if x == 'c':
+            charge *= 2
+        else:
+            damage += charge
+    return damage
+
+
+def findSwaps(tol, st):
+    swaps = 0
+    d = calcDamage(st)
+    while d > tol:
+        st = swap(st)
+        if st == 'impossible':
+            return -1
+        else:
+            swaps += 1
+        d = calcDamage(st)
+    return swaps
+
+
+def swap(st):
+    st = st[::-1]
+    first, second = ''
+    for i, x in enumerate(st):
+        second = x
+        if (first + second == 'cs'):
+            tmp = st[i]
+            st[i] = st[i-1]
+            st[i-1] = tmp
+            return st[::-1]
+        first = second
+    return 'impossible'
+
+
+t = int(input())  # read a line with a single integer
+for i in range(1, t + 1):
+        # read a list of integers, 2 in this case
+    d = int(input().split(' ')[0])
+    st = input().split(' ')[1]
+    ans = findSwaps(d, st)
+    if ans == -1:
+        ans = "IMPOSSIBLE"
+    print("Case #{}: {}".format(i, ans))
+    # check out .format's specification for more formatting options
