fcc38bbebf906a4e76c09f7f538781a514cf2495
ezhang7423 <ezhang7423@student.palomar.edu>
2020-04-08 11:25:11 -0700

    submit maxprod

--- hw/maxprod.py
+++ hw/maxprod.py
@@ -3,7 +3,11 @@ import math
 
 def maxProduct(nums):
     B = nums[::-1]
-       for i in range(1, len(nums)):
-            nums[i] *= nums[i - 1] or 1
-            B[i] *= B[i - 1] or 1
-        return max(nums + B)
+    for i in range(1, len(nums)):
+        nums[i] *= nums[i - 1] or 1
+        B[i] *= B[i - 1] or 1
+    print(nums, B)
+    return max(nums + B)
+
+
+maxProduct([0, 1, 2, 3, 0])
